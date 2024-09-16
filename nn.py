b6be54548b84832260d64dac1205faeb67557cd8
Cutty-sark <gmespaceprogram@gmail.com>
2024-09-16 13:52:18 +0100

    Block class created

--- nn.py
+++ nn.py
@@ -6,5 +6,18 @@ if __name__ == "__main__":
     keyboard.wait('esc')
 
 
-class block():
+class Block():
+    def __init__(self, title):
+        self.title = title
+        self.content = []
     
+    def  add_content(self, text):
+        self.content.append(text)
+
+    def display(self):
+        print(f"{'_'*50}")
+        print(f"|{self.title.center(48)}|")
+        print(f"|{'-'*48}|")
+        for line in self.content:
+            print(f"| {line.ljust(47)}|")
+        print(f"|{'_'*48}|")
